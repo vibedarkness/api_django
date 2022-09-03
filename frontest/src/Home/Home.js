4acab0200a59e787c81efd1be4afa4d3944ea7fb
devdingo <omarthioms35@gmail.com>
2022-09-03 21:55:15 +0000

    react

---
+++ frontest/src/Home/Home.js
@@ -0,0 +1,15 @@
+
+import './Home.css';
+
+const Home = () => {
+    return ( 
+        <div className="Home">
+            <div className="contenu">
+
+            </div>
+            
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ frontest/src/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import "../index.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div>
+        <a href="" className="logo">
+          Omzo Test
+        </a>
+      </div>
+      <ul className="liens">
+        <li>
+          <a href="" className="lien">
+            Acceuil
+          </a>
+        </li>
+        <li>
+          <a href="" className="lien">
+            Ajouter
+          </a>
+        </li>
+        <li>
+          <a href="" className="lien">
+            Se Connecter
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
