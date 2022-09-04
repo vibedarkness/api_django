1b64b14038c5069b898169b0340494e8627f8703
devdingo <omarthioms35@gmail.com>
2022-09-04 13:55:20 +0000

    react

--- frontest/src/Home/Home.js
+++ frontest/src/Home/Home.js
@@ -1,15 +1,45 @@
-
-import './Home.css';
+import "./Home.css";
 
 const Home = () => {
-    return ( 
-        <div className="Home">
-            <div className="contenu">
+  return (
+    <div className="Home">
+      <div className="contenu">
+        {/* <div class="product-card">
+          <h1>Best Shoes</h1>
+          <p>Lorem ipsum, or lipsum as it is sometimes known</p>
+          <div class="product-pic"></div>
+          <div class="product-colors">
+            <span
+              class="blue active"
+              data-color="#7ed6df"
+              data-pic="url(1.png)"
+            ></span>
+            <span
+              class="green"
+              data-color="#badc58"
+              data-pic="url(2.png)"
+            ></span>
+            <span
+              class="yellow"
+              data-color="#f9ca24"
+              data-pic="url(3.png)"
+            ></span>
+            <span
+              class="rose"
+              data-color="#ff7979"
+              data-pic="url(4.png)"
+            ></span>
+          </div>
+          <div class="product-info">
+            <div class="product-price">$90</div>
+            <a href="#" class="product-button">
+              Add to Cart
+            </a>
+          </div>
+        </div> */}
+      </div>
+    </div>
+  );
+};
 
-            </div>
-            
-        </div>
-     );
-}
- 
 export default Home;
